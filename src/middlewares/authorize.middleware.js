20ec64e34b43bf3ad6d936455de7aa005e3bc197
ashish <guptashish101020@gmail.com>
2026-02-07 02:58:17 +0530

    l

--- src/middlewares/authorize.middleware.js
+++ src/middlewares/authorize.middleware.js
@@ -1,11 +1,10 @@
-
 const authorize = (roles = []) => {
-    return (req, res, next) => {
-        if (req.user.role !== roles) {
-            return res.status(403).json({ message: 'Access denied: insufficient permissions' });
-        }
-        next();
-    };
-}
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access denied: insufficient permissions' });
+    }
+    next();
+  };
+};
 
 module.exports = { authorize };
